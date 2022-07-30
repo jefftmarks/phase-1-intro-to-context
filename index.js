ef0aae1842efd3cba42d45979cda60f508365ef5
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-30 08:33:47 -0400

    commit tree 6a707c03a5221a7dca1c0960ea502f3c76048db4

--- index.js
+++ index.js
@@ -36,42 +36,78 @@ function createTimeOutEvent(employee, date) {
     return employee;
 }
 
-function hoursWorkedOnDate(employee, date) {
-    let clockIn;
-    let clockOut;
-    employee.timeInEvents.forEach((element) => {
-        if (element.date === date) {
-            clockIn = element.hour;
-        }
-    })
-    employee.timeOutEvents.forEach((element) => {
-        if (element.date === date) {
-            clockOut = element.hour;
-        }
-    })
-    return (clockOut - clockIn) / 100;
-}
+//my function
+// function hoursWorkedOnDate(employee, date) {
+//     let clockIn;
+//     let clockOut;
+//     employee.timeInEvents.forEach((element) => {
+//         if (element.date === date) {
+//             clockIn = element.hour;
+//         }
+//     })
+//     employee.timeOutEvents.forEach((element) => {
+//         if (element.date === date) {
+//             clockOut = element.hour;
+//         }
+//     })
+//     return (clockOut - clockIn) / 100;
+// }
 
-function wagesEarnedOnDate(employee, date) {
-    const hours = hoursWorkedOnDate(employee, date);
-    return hours * employee.payPerHour;
-}
-
-function allWagesFor(employee) {
-    const timeIn = employee.timeInEvents;
-    const timeOut = employee.timeOutEvents;
-    let totalWages = 0
-    for (let i = 0; i < timeIn.length; i++) {
-        for (let j = 0; j < timeOut.length; j++) {
-            if (timeIn[i].date === timeIn[j].date) {
-                let dailyWages = wagesEarnedOnDate(employee, timeIn[i].date);
-                totalWages += dailyWages;
-            }
-        }
+    //solution
+    let hoursWorkedOnDate = function(employee, soughtDate){
+        let inEvent = employee.timeInEvents.find(function(e){
+            return e.date === soughtDate
+        })
+    
+        let outEvent = employee.timeOutEvents.find(function(e){
+            return e.date === soughtDate
+        })
+    
+        return (outEvent.hour - inEvent.hour) / 100
     }
-    return totalWages;
-}
 
+
+// my solution
+// function wagesEarnedOnDate(employee, date) {
+//     const hours = hoursWorkedOnDate(employee, date);
+//     return hours * employee.payPerHour;
+// }
+
+        //solution
+        function wagesEarnedOnDate(employee, date) {
+            const rawWage = hoursWorkedOnDate(employee, date) * employee.payPerHour;
+            return parseFloat(rawWage.toString())
+        }
+
+//my solution
+// function allWagesFor(employee) {
+//     const timeIn = employee.timeInEvents;
+//     const timeOut = employee.timeOutEvents;
+//     let totalWages = 0
+//     for (let i = 0; i < timeIn.length; i++) {
+//         for (let j = 0; j < timeOut.length; j++) {
+//             if (timeIn[i].date === timeIn[j].date) {
+//                 let dailyWages = wagesEarnedOnDate(employee, timeIn[i].date);
+//                 totalWages += dailyWages;
+//             }
+//         }
+//     }
+//     return totalWages;
+// }
+
+        //solution
+        function allWagesFor(employee) {
+            let elligibleDates = employee.timeInEvents.map((element) => {
+                return element.date;
+            });
+            let payable = elligibleDates.reduce((accum, currentVal) => {
+                return accum + wagesEarnedOnDate(employee, currentVal);
+            }, 0);
+
+            return payable;
+        }
+
+// my solution        
 function calculatePayroll(records) {
     let wagesOwed = 0;
     records.forEach(employee => {
@@ -79,3 +115,10 @@ function calculatePayroll(records) {
     })
     return wagesOwed;
 }
+
+    //solution
+    function calculatePayRoll(records) {
+        return records.reduce((accum, currentVal) => {
+            return accum + allWagesFor(currentVal);
+        }, 0);
+    }
